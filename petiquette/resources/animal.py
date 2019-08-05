a211322eee7ed550b9a602fe33f998304a0d7359
Derek Michael Frank <derek@fanai.io>
2019-08-05 12:14:42 -0700

    chore(resources): Add some schema for outputs

--- petiquette/resources/animal.py
+++ petiquette/resources/animal.py
@@ -1,11 +1,46 @@
+import random
+import uuid
+from dataclasses import field
+
+import marshmallow as mm
 from marshmallow.fields import (
     UUID,
 )
+from marshmallow_dataclass import dataclass
+from typing import (
+    ClassVar,
+    Optional,
+    Type,
+)
 
 
-def dogs():
-    return []
+DOG_TYPES_TO_ABBREVIATIONS = {
+    'Australian Cattle Dog': 'ACD',
+    'German Shepherd Dog': 'GSD',
+    'Pomeranian': None,
+}
 
 
-def dog(id: UUID()):
-    return {'id': str(id)}
+@dataclass
+class Dog:
+    """Dog type contract."""
+    id: str = field(metadata={'marshmallow_field': UUID()})
+    name: str
+    abbreviation: Optional[str]
+    Schema: ClassVar[Type[mm.Schema]]
+
+
+def dogs() -> Dog.Schema(many=True):
+    return [
+        Dog(id=uuid.uuid1(), name=name, abbreviation=abbrev)
+        for name, abbrev in (
+            random.choice(list(DOG_TYPES_TO_ABBREVIATIONS.items()))
+            for idx in range(10)
+        )
+    ]
+
+
+def dog(id: UUID()) -> Dog.Schema():
+    name = random.choice(list(DOG_TYPES_TO_ABBREVIATIONS))
+    abbrev = DOG_TYPES_TO_ABBREVIATIONS[name]
+    return Dog(id=id, name=name, abbreviation=abbrev)
--- petiquette/resources/pet.py
+++ petiquette/resources/pet.py
@@ -1,6 +1,22 @@
+from dataclasses import field
+
+import marshmallow as mm
 from marshmallow.fields import (
     UUID,
 )
+from marshmallow_dataclass import dataclass
+from typing import (
+    ClassVar,
+    Type,
+)
+
+
+@dataclass
+class Pet:
+    """Pet contract."""
+    id: str = field(metadata={'marshmallow_field': UUID()})
+    type: str
+    Schema: ClassVar[Type[mm.Schema]] = mm.Schema
 
 
 def pets():
